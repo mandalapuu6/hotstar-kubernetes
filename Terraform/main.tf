3796bf27ea1e3680259496554f9598e2078d32ee
AaryRajpoot <ronit1336@gmail.com>
2025-11-16 17:31:24 +0530

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -7,19 +7,65 @@ terraform {
   }
 }
 
-# Configure the AWS Provider
 provider "aws" {
-  region = "ap-south-1"
+  region     = "ap-south-1"
   access_key = var.access_key
   secret_key = var.secret_key
 }
 
-# create security group for the ec2 instance
-resource "aws_security_group" "ec2_security_group" {
-  name        = "ec2 security group"
-  description = "allow access on ports 22"
+#############################################
+# VPC + SUBNET + IGW + ROUTE TABLE
+#############################################
 
-  # allow access on port 22
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "main-vpc"
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-subnet"
+  }
+}
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "public-route-table"
+  }
+}
+
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+#############################################
+# SECURITY GROUP
+#############################################
+
+resource "aws_security_group" "ec2_security_group" {
+  name        = "ec2-security-group"
+  description = "Allow SSH"
+  vpc_id      = aws_vpc.main.id
+
   ingress {
     description = "ssh access"
     from_port   = 22
@@ -31,21 +77,27 @@ resource "aws_security_group" "ec2_security_group" {
   egress {
     from_port   = 0
     to_port     = 0
-    protocol    = -1
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
-    Name = "Monitoring server security group"
+    Name = "Monitoring server SG"
   }
 }
 
+#############################################
+# EC2 INSTANCE
+#############################################
+
 resource "aws_instance" "Monitoring_server" {
-ami = "ami-00bb6a80f01f03502"  
-instance_type = "t2.medium"
-security_groups = [aws_security_group.ec2_security_group.name]
-key_name = var.key_name
-tags = {
-  Name: var.instance_name
-}
+  ami                    = "ami-00bb6a80f01f03502"
+  instance_type          = "t2.medium"
+  subnet_id              = aws_subnet.public_subnet.id
+  vpc_security_group_ids = [aws_security_group.ec2_security_group.id]
+  key_name               = var.key_name
+
+  tags = {
+    Name = var.instance_name
+  }
 }
